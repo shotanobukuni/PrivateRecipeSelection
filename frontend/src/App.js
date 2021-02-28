04f053348d4d439135e3789f648ae190cff95817
shota.nobukuni <gsnysh4073@gmail.com>
2021-02-28 21:36:26 +0900

    update : frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h2>食べたいものが決まらない?</h2>
+        <h2>そんな時は下をクリック!!</h2>
       </header>
     </div>
   );
