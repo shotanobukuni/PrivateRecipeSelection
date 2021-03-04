41e6f9cb1e2fb24ea63deefdf16d7b020e69c19d
shota.nobukuni <gsnysh4073@gmail.com>
2021-03-04 23:32:52 +0900

    add to get api feature

--- frontend/src/components/RecipeList.js
+++ frontend/src/components/RecipeList.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 function RecipeList() {
+    const APIVersion='v1'
+    const [recipes, setRecipes] = useState([])
+    useEffect(() => {
+        axios.get(`http://127.0.0.1:8000/api/${APIVersion}/recipes/`)
+        .then(res => {setRecipes(res.data)})
+    }, [])
     return (
         <div>
             <p>レシピ一覧</p>
+            <ul>
+                {
+                    recipes.map(recipe => <li>
+                        レシピ名 : {recipe.name}
+                        <br />
+                        内容 : {recipe.context}
+                    </li>)
+                }
+            </ul>
         </div>
     )
 }
